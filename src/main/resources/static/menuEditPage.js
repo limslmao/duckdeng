2766e366260fac57d466ee45b19ec30108bb63f8
Arte <Arte.lillard@ibm.com>
2023-08-07 13:57:43 +0800

    20230807 v1

--- src/main/resources/static/menuEditPage.js
+++ src/main/resources/static/menuEditPage.js
@@ -1,26 +1,23 @@
-function myFunction() {
-  document.getElementById("demo").innerHTML = "Hello World";
-  console.log("success")
+var addItemClickCount = 0
+var menuArr =  ["全鴨兩吃","600","checked","650"]
+$(function(){
+    queryItem()
+    $('#addItem').on('click', addMenuItem);
+});
+function queryItem() {
+    var newRow = $('<tr>');
+    newRow.append('<td>' + menuArr[0] + '</td>');
+    newRow.append('<td>' + menuArr[1] + '</td>');
+    newRow.append('<td><input class="custom-checkbox" type="checkbox"  ' + menuArr[2] + '></td>');
+    newRow.append('<td>' + menuArr[3] + '</td>');
+    $('#tableBody').append(newRow);
 }
-
- function changeContent(page) {
-            // Fetch the content for the selected page
-            fetch(page)
-                .then(response => response.text())
-                .then(data => {
-                    // Update the content wrapper with the new content
-                    document.getElementById('content-wrapper').innerHTML = data;
-                });
-        }
-
-        // Add event listeners to the side menu items
-        document.addEventListener('DOMContentLoaded', function() {
-            const sideMenuItems = document.querySelectorAll('.sidebar-menu a');
-            sideMenuItems.forEach(item => {
-                item.addEventListener('click', function(event) {
-                    event.preventDefault();
-                    const page = event.target.getAttribute('href');
-                    changeContent(page);
-                });
-            });
-        });
+function addMenuItem() {
+    addItemClickCount++; // Increment the counter
+    var newRow = $('<tr>');
+    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入品項名稱" id="itemName_' + addItemClickCount + '"></td>');
+    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入品項金額" id="itemPrice_' + addItemClickCount + '"></td>');
+    newRow.append('<td><input class="custom-checkbox" type="checkbox" id="itemSpCheck_' + addItemClickCount + '"></td>');
+    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入香鍋麻辣加購價" id="itemSpPrice_' + addItemClickCount + '"></td>');
+    $('#tableBody').append(newRow);
+}
