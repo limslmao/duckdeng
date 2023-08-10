121d5802e8ee636934a3a38e6dacbc40e9400120
Arte <Arte.lillard@ibm.com>
2023-08-10 15:40:39 +0800

    20230810

--- src/main/resources/static/menuEditPage.js
+++ src/main/resources/static/menuEditPage.js
@@ -1,22 +1,24 @@
-const jsonEx = {
-                 "orderItems": [
+let initJson = {
+                 "menuDtl": [
                    {
-                     "menuId": "001",
+                     "itemId": "001",
                      "price":600,
                      "itemType":"main",
                      "itemIngred":"duck",
                      "itemUnit":"full",
                      "itemSpicy":"N",
-                     "itemCookMethod":"two"
+                     "itemCookMethod":"two",
+                     "note": "全鴨二吃"
                    },
                     {
-                     "menuId": "002",
+                     "itemId": "002",
                      "price":650,
                      "itemType":"main",
                      "itemIngred":"duck",
                      "itemUnit":"full",
                      "itemSpicy":"Y",
-                     "itemCookMethod":"two"
+                     "itemCookMethod":"two",
+                     "note": "全鴨二吃"
                    }
                  ]
                }
@@ -36,16 +38,35 @@ const orderConvert = {
   '013': '荷葉餅一份',
   '014': '甜麵醬一份'
 };
-var addItemClickCount = 0
-var menuArr =  ["全鴨兩吃","600","主食","650"]
 $(function(){
-    queryItem()
+    getData()
     $('#addItem').on('click', addMenuItem);
+    $('#tableBody').on('click', '.cancel-button', function() {
+        cancelItemAdd()
+    });
+    $('#tableBody').on('click', '.confirm-button', function() {
+        confirmItemAdd()
+    });
 });
+function getData() {
+    $.ajax({
+      type: 'GET',
+      url: '/orderItems ',
+      success: function(response) {
+        $('#loading').attr('hidden', true);
+        console.log('Response:', JSON.stringify(response, null, 2));
+        initJson = response
+        queryItem()
+      },
+      error: function(xhr, status, error) {
+        console.log('Error:', error);
+      }
+    });
+}
 function queryItem() {
-    for (const item of jsonEx.orderItems) {
+    for (const item of initJson.menuDtl) {
         var newRow = $('<tr>');
-        newRow.append('<td>' + orderConvert[item.menuId] + '</td>');
+        newRow.append('<td>' + orderConvert[item.itemId] + '</td>');
         newRow.append('<td>' + item.price + '</td>');
         newRow.append('<td>' + item.itemType + '</td>');
         newRow.append('<td>' + item.itemIngred + '</td>');
@@ -57,11 +78,24 @@ function queryItem() {
     }
 }
 function addMenuItem() {
-    addItemClickCount++; // Increment the counter
-    var newRow = $('<tr>');
-    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入品項名稱" id="itemName_' + addItemClickCount + '"></td>');
-    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入品項金額" id="itemPrice_' + addItemClickCount + '"></td>');
-    newRow.append('<td><input class="custom-checkbox" type="checkbox" id="itemSpCheck_' + addItemClickCount + '"></td>');
-    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入香鍋麻辣加購價" id="itemSpPrice_' + addItemClickCount + '"></td>');
+    $('#addItem').prop('disabled', true);
+//    addItemClickCount++; // Increment the counter
+    var newRow = $('<tr id = "createItem">');
+    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入品項名稱" id="itemName"></td>');
+    newRow.append('<td><input type="text" class="form-control" placeholder="請輸入品項金額" id="itemPrice"></td>');
+    newRow.append('<td><select class="form-select" id="itemType"><option value="main">主菜</option><option value="add">加購</option></select></td>');
+    newRow.append('<td><select class="form-select" id="itemType"><option value="duck">鴨</option><option value="chicken">雞</option></select></td>');
+    newRow.append('<td><select class="form-select" id="half"><option value="main">半隻</option><option value="full">全隻</option></select></td>');
+    newRow.append('<td><select class="form-select" id="itemType"><option value="two">兩吃</option><option value="saute ">剁炒</option>><option value="cut">切盤</option><option value="else">其他</option></select></td>');
+    newRow.append('<td><input class="custom-checkbox" type="checkbox" id="itemSpCheck"></td>');
+    newRow.append('<td><button class="btn btn-primary cancel-button">取消</button></td>');
+    newRow.append('<td><button class="btn btn-primary confirm-button">確定</button></td>');
     $('#tableBody').append(newRow);
+}
+function cancelItemAdd() {
+    $('#addItem').prop('disabled', false);
+    $('#createItem').remove();
+}
+function confirmItemAdd() {
+
 }
--- src/main/resources/static/orderQueryPage.js
+++ src/main/resources/static/orderQueryPage.js
@@ -1,4 +1,4 @@
-const jsonEx = {
+let initJson = {
   "orderDtl": [
     {
       "orderId": "20230808001",
@@ -7,7 +7,7 @@ const jsonEx = {
         "002": 1,
         "003": 2
       },
-      "totalPrice": 670
+      "totalAmount": 670
     },
     {
       "orderId": "20230808002",
@@ -16,11 +16,10 @@ const jsonEx = {
         "002": 1,
         "003": 2
       },
-      "totalPrice": 670
+      "totalAmount": 670
     }
   ]
 };
-
 const orderConvert = {
   '001': '全鴨二吃',
   '002': '全鴨二吃香鍋麻辣',
@@ -38,30 +37,83 @@ const orderConvert = {
   '014': '甜麵醬一份'
 };
 
-$(function() {
-  getOrderData();
+$(document).ready(function() {
+$('#search').on('click', function(event) {
+    $('#loading').removeAttr('hidden');
+    event.preventDefault(); // Prevent the default link navigation
+    getOrderData(); // Call your function
+});
+$('.date-btn').on('click', function(event) {
+    event.preventDefault(); // Prevent the default link navigation
+    var clickedButtonId = $(this).attr('id');
+    getDate(clickedButtonId)
+});
 });
 
+
+function getDate(clickedButtonId) {
+    const today = new Date();
+    const year = today.getFullYear();
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    let day = today.getDate();
+    switch (clickedButtonId) {
+        case 'today':
+            day = String(day).padStart(2, '0');
+            break;
+        case 'yesterday':
+            day = String(day - 1).padStart(2, '0');
+            break;
+        case 'thisWeek':
+            const todayOfWeek = new Date(today);
+            const dayOfWeek = today.getDay(); // 0 (Sunday) to 6 (Saturday)
+            todayOfWeek.setDate(day - dayOfWeek + 1); // Adjust for the beginning of the week
+            const formattedStartDate = `${year}${month}${String(todayOfWeek.getDate()).padStart(2, '0')}`;
+            todayOfWeek.setDate(day + (7 - dayOfWeek)); // Adjust for the end of the week
+            const formattedEndDate = `${year}${month}${String(todayOfWeek.getDate()).padStart(2, '0')}`;
+            $('#startDate').val(formattedStartDate);
+            $('#endDate').val(formattedEndDate);
+            return; // 不再输出 formattedDate
+        default:
+            break;
+    }
+    const formattedDate = `${year}${month}${day}`;
+    $('#startDate').val(formattedDate);
+    $('#endDate').val(formattedDate);
+}
 function getOrderData() {
-  console.log(jsonEx);
-  contentHtmlInner();
+var startDate = $('#startDate').val()||"20230810"
+var endDate = $('#endDate').val()||""
+$.ajax({
+  type: 'GET',
+  url: '/orderDetails?startDate='+startDate+'&endDate='+endDate+' ',
+  success: function(response) {
+    $('#loading').attr('hidden', true);
+    console.log('Response:', JSON.stringify(response, null, 2));
+    initJson = response
+    contentHtmlInner();
+  },
+  error: function(xhr, status, error) {
+    console.log('Error:', error);
+  }
+});
+
 }
 
 function contentHtmlInner() {
   $('#dataContext').empty(); // 清空数据，避免重复添加
   let count = 0;
-  for (const order of jsonEx.orderDtl) {
+  for (const order of initJson.orderDtl) {
     const newRow = $('<tr>');
     newRow.append('<td>' + order.orderId + '</td>');
     newRow.append('<td>' + orderContentCreate(count) + '</td>');
-    newRow.append('<td>' + order.totalPrice + '</td>');
+    newRow.append('<td>' + order.totalAmount + '</td>');
     $('#dataContext').append(newRow);
     count++;
   }
 }
 function orderContentCreate(count) {
     let tdHtml = '';
-    const orderItem = jsonEx.orderDtl[count].orderItem; // 获取当前订单的订单项
+    const orderItem = initJson.orderDtl[count].orderItem;
     for (const key in orderItem) {
         if (orderConvert.hasOwnProperty(key)) {
             tdHtml += '<tr><td>' + orderConvert[key] + '</td><td>' + orderItem[key] + '</td></tr>';
