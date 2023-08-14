3fa05d7feaace92ac02c16c16c7666518fe8c24f
Arte <Arte.lillard@ibm.com>
2023-08-14 10:11:33 +0800

    report fix

--- src/main/resources/static/duckHomePage.js
+++ src/main/resources/static/duckHomePage.js
@@ -1,40 +1,85 @@
+
 $(function(){
 //    chart()
   $('#createSheet').on('click', function(event) {
+
     event.preventDefault(); // Prevent the default link navigation
-    chart(); // Call your function
-    calculateDateRange();
+    getData()
+//    chart(); // Call your function
   });
   $('#dateRange').on('change', function(event) {
       event.preventDefault(); // Prevent the default link navigation
-      dateRange(); // Call your function
+      updateDateRangeText(); // Call your function
     });
 
 });
 
-const data = {//給他的
-    startDate:"",
-    endDate:"",
-    rangeDate:"",//week,month,year
-    type:"" //itemIngred itemUnit itemCookMethod itemSpicy
+const dataForCondition = {//給他的
+     startDate: "20230211",
+     endDate: "20230811",
+     rangeType: "week",//week,month,year
+     type: "itemIngred"//itemIngred itemUnit itemCookMethod itemSpicy
 }
-const data_ = {
-    {$rangeDate}:[
-        range_1:{
-            {$type1}:"count"
-            {$type2}:"count"
-        },
-        range_2:{
-            {$type1}:"count"
-            {$type2}:"count"
-        }
-    ],
-    totalCount:{
-        {$type1}:"totalCount"
-        {$type2}:"totalCount"
-    }
+const dataForResponse = {
+   dataType:["duck","chicken"],
+   rangeType:"week",
+   countDtl: [
+     {
+       range:"Jan-1",
+       count: {
+         duck: 100,
+         chicken: 100
+       }
+     },
+     {
+       range:"Jan-2",
+       count: {
+         duck: 120,
+         chicken: 80
+       }
+     },
+     {
+       range:"Jan-3",
+       count: {
+         duck: 90,
+         chicken: 80
+       }
+     },
+     {
+       range:"Jan-4",
+       count: {
+         duck: 14,
+         chicken: 80
+       }
+     },
+     {
+       range:"Feb-1",
+       count: {
+         duck: 13,
+         chicken: 80
+       }
+     },
+     {
+       range:"Feb-2",
+       count: {
+         duck: 10,
+         chicken: 80
+       }
+     },
+     {
+       range:"Feb-3",
+       count: {
+         duck: 11,
+         chicken: 80
+       }
+     }
+   ],
+   totalCount: {
+     duck: 220,
+     chicken: 180
+   }
 }
-function dateRange() {
+function updateDateRangeText() {
     var text = '';
     switch ($('#dateRange').val()) {
         case 'week':
@@ -52,63 +97,112 @@ function dateRange() {
     }
     $('#range').text(text)
 }
-function calculateDateRange() {
-    var today = new Date();
-    var halfYearAgo = new Date();
+function getStartDateAndEndDate(dateRange) {
+  var today = new Date();
+  var startDate, endDate;
 
-    // 設定半年前的日期
-    halfYearAgo.setMonth(today.getMonth() - 6);
+  if (dateRange === 'week') {
+    var halfYearAgo = new Date(today.getFullYear(), today.getMonth() - 6, today.getDate());
+    startDate = halfYearAgo;
+    endDate = today;
+  } else if (dateRange === 'month') {
+    var startOfYear = new Date(today.getFullYear(), 0, 1);
+    var endOfYear = new Date(today.getFullYear(), 11, 31);
+    startDate = startOfYear;
+    endDate = endOfYear;
+  } else if (dateRange === 'year') {
+    var fiveYearsAgo = new Date(today.getFullYear() - 4, 0, 1);
+    var endOfYear = new Date(today.getFullYear(), 11, 31);
+    startDate = fiveYearsAgo;
+    endDate = endOfYear;
+  }
 
-    // 將日期格式化為 "YYYY-MM-DD" 格式
-    var todayFormatted = formatDate(today);
-    var halfYearAgoFormatted = formatDate(halfYearAgo);
+  var formattedStartDate = formatDateToYYYYMMDD(startDate);
+  var formattedEndDate = formatDateToYYYYMMDD(endDate);
 
-    var dateRange = halfYearAgoFormatted + ' 到 ' + todayFormatted;
-    console.log(dateRange);
+  return {
+    startDate: formattedStartDate,
+    endDate: formattedEndDate
+  };
 }
-function formatDate(date) {
+function formatDateToYYYYMMDD(date) {
     var year = date.getFullYear();
     var month = (date.getMonth() + 1).toString().padStart(2, '0');
     var day = date.getDate().toString().padStart(2, '0');
-    return year + month+ day;
+    return year  + month  + day;
+}
+
+function getData() {
+    dateRange = $('#dateRange').val();
+    dateType = $('#dateType').val();
+    var weekDateRange = getStartDateAndEndDate(dateRange);
+    dataForCondition.startDate =  weekDateRange.startDate;
+    dataForCondition.endDate =  weekDateRange.endDate;
+    dataForCondition.rangeType =  dateRange;
+    dataForCondition.type = dateType;
+    console.log(dataForCondition);
+//ajax input dataForCondition...
+
+    var countDtl = dataForResponse.countDtl;
+    var dataKey = dataForResponse.dataType;
+    var dataX = [];
+    var dataCounts = [];
+    for (var i = 0; i < countDtl.length; i++) {
+        dataX.push(countDtl[i].range);
+        var countData = [];
+        for (var j = 0; j < dataKey.length; j++) {
+            countData.push(countDtl[i].count[dataKey[j]]);
+        }
+        dataCounts.push(countData);
+    }
+    var nullBarChartCount = searchNullCanvas()
+    console.log("nullBarChartCount:"+nullBarChartCount)
+    if (nullBarChartCount) {
+        chart(dataX, dataCounts, dataKey,nullBarChartCount);
+    }
+
 }
 
 
 
+function searchNullCanvas() {
+    return false;
+    for (var i = 1; i <= 3; i++) {
+        var styleAttribute = $('#barChart-' + i).attr('style'); ///要找出如何判斷是否有插入圖表的QUERY
+        if (styleAttribute && styleAttribute.includes('box-sizing')) {
+        } else {
+        return i;
+        }
+    }
+    alert("請刪除一個項目")
+    return null
+}
+function chart(dataX, dataCounts, dataKey, nullBarChartCount) {
+    var barChartCanvas = $('#barChart-' + nullBarChartCount).get(0).getContext('2d');
+    var axisX = dataX;
+    var datasets = [];
 
+    for (var i = 0; i < dataKey.length; i++) {
+        var color = `rgba(${(i * 47) % 255}, ${(i * 71) % 255}, ${(i * 113) % 255}, 0.9)`;
 
+        datasets.push({
+            label: dataKey[i],
+            backgroundColor: color,
+            borderColor: 'rgba(60,141,188,0.8)',
+            pointRadius: false,
+            pointColor: '#3b8bba',
+            pointStrokeColor: 'rgba(60,141,188,1)',
+            pointHighlightFill: '#fff',
+            pointHighlightStroke: 'rgba(60,141,188,1)',
+            data: dataCounts.map(countData => countData[i])
+        });
+    }
 
-function chart() {
-    var barChartCanvas = $('#barChart').get(0).getContext('2d');
-    var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
-    var data_count_chicken = [20,10,20,20,20,30,20,15,30,20,20,20]
-    var data_count_duck = [10,20,20,20,30,20,15,30,20,20,20,20,]
-    // 数据
     var data = {
-        labels: months,
-        datasets: [{
-            label: 'Duck',
-            backgroundColor: 'rgba(255, 99, 132, 0.9)', // 柱状图颜色
-            borderColor: 'rgba(60,141,188,0.8)',
-            pointRadius: false,
-            pointColor: '#3b8bba',
-            pointStrokeColor: 'rgba(60,141,188,1)',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(60,141,188,1)',
-            data: data_count_chicken // Y 轴数据
-        },{
-            label: 'Chicken',
-            backgroundColor: 'rgba(60,141,188,0.9)', // 柱状图颜色
-            borderColor: 'rgba(60,141,188,0.8)',
-            pointRadius: false,
-            pointColor: '#3b8bba',
-            pointStrokeColor: 'rgba(60,141,188,1)',
-            pointHighlightFill: '#fff',
-            pointHighlightStroke: 'rgba(60,141,188,1)',
-            data: data_count_duck // Y 轴数据
-        }]
+        labels: axisX,
+        datasets: datasets
     };
-    // 配置选项
+
     var options = {
         responsive: true,
         maintainAspectRatio: false,
@@ -125,7 +219,7 @@ function chart() {
             }
         }
     };
-    // 创建柱状图
+
     var barChart = new Chart(barChartCanvas, {
         data: data,
         type: 'bar',
