88c8809fe78fb8840a86803e7baeba1db3ce9271
limslmao <jamesmau87@gmail.com>
2023-09-01 17:59:15 +0800

    add orderPlatformId compare between DB and json

--- src/main/java/com/duckdeng/orderstat/lim/service/strategy/FoodPandaOrderImporter.java
+++ src/main/java/com/duckdeng/orderstat/lim/service/strategy/FoodPandaOrderImporter.java
@@ -3,7 +3,6 @@ package com.duckdeng.orderstat.lim.service.strategy;
 import com.duckdeng.orderstat.lim.model.OrderDetail;
 import com.duckdeng.orderstat.lim.model.OrderPlatformDetail;
 import com.google.api.core.ApiFuture;
-import com.google.cloud.firestore.FieldPath;
 import com.google.cloud.firestore.Firestore;
 import com.google.cloud.firestore.QueryDocumentSnapshot;
 import com.google.cloud.firestore.QuerySnapshot;
@@ -29,21 +28,24 @@ public class FoodPandaOrderImporter implements OrderImporter {
         // Step 2. 利用批次資料做一個完整的Transaction
         ApiFuture<Void> future = dbFirestore.runTransaction(transaction -> {
             Map<String, Integer> maxIdsForDate = new HashMap<>();
+            Map<String, List<String>> existingOrderPlatformIdsByDate = new HashMap<>();
 
-            // 針對每一個日期，取該日期最後一碼ID，並放入Map
             for (String date : groupedOrders.keySet()) {
                 String datePart = date.substring(1);
 
+                // Step 1. 獲取已存在的orderPlatformIds
                 ApiFuture<QuerySnapshot> future1 = transaction.get(dbFirestore.collection("orderDetail")
-                        .whereGreaterThanOrEqualTo(FieldPath.documentId(), datePart + "001")
-                        .whereLessThanOrEqualTo(FieldPath.documentId(), datePart + "999"));
+                        .whereEqualTo("orderDate", date));
+                List<QueryDocumentSnapshot> documentsForIdCheck = future1.get().getDocuments();
+                List<String> existingOrderPlatformIds = documentsForIdCheck.stream()
+                        .map(doc -> doc.getString("orderPlatformId"))
+                        .collect(Collectors.toList());
+                existingOrderPlatformIdsByDate.put(date, existingOrderPlatformIds);
 
-                List<QueryDocumentSnapshot> documents = future1.get().getDocuments();
-                String highestOrderID = documents.stream()
+                String highestOrderID = documentsForIdCheck.stream()
                         .map(QueryDocumentSnapshot::getId)
                         .max(String::compareTo)
                         .orElse(datePart + "000");
-
                 int startID = Integer.parseInt(highestOrderID.substring(7));
                 maxIdsForDate.put(date, startID);
             }
@@ -52,7 +54,13 @@ public class FoodPandaOrderImporter implements OrderImporter {
                 String datePart = entry.getKey().substring(1);
                 int startID = maxIdsForDate.get(entry.getKey());
 
-                for (OrderPlatformDetail orderDetailDTO : entry.getValue()) {
+                // Step 2. 過濾掉已存在的訂單
+                List<OrderPlatformDetail> toBeInserted = entry.getValue().stream()
+                        .filter(order -> !existingOrderPlatformIdsByDate.get(entry.getKey()).contains(order.getOrderPlatformId()))
+                        .toList();
+
+                // Step 3. 插入新的訂單
+                for (OrderPlatformDetail orderDetailDTO : toBeInserted) {
                     String orderItemKey = datePart + String.format("%03d", ++startID);
 
                     OrderDetail orderDetail = new OrderDetail();
@@ -69,6 +77,7 @@ public class FoodPandaOrderImporter implements OrderImporter {
                     transaction.set(dbFirestore.collection("orderDetail").document(orderItemKey), orderDetail);
                 }
             }
+
             return null;
         });
 
